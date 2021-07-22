4939d08b8206ccf1ce642ebf4ad043698b46bced
Joaquin Vivo <joaquinvivo@gmail.com>
2021-07-22 16:04:39 -0300

    initial commit

---
+++ lib/signup/view/signup_page.dart
@@ -0,0 +1,27 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_firebase_login/signup/cubit/signup_cubit.dart';
+import 'package:flutter_firebase_login/signup/view/signup_form.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  static Route route() {
+    return MaterialPageRoute<void>(builder: (_) => const SignUpPage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sign Up')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: BlocProvider<SignUpCubit>(
+          create: (_) => SignUpCubit(context.read<AuthenticationRepository>()),
+          child: const SignUpForm(),
+        ),
+      ),
+    );
+  }
+}
